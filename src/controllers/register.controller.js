3d51b0008da73f4b017b2b26b7d889cb391eae76
silvanarl <ramossilvana495@gmail.com>
2020-06-02 12:49:34 -0500

    funcionalidad ok
    auth completado email y password

--- src/controllers/register.controller.js
+++ src/controllers/register.controller.js
@@ -1,11 +1,36 @@
-import { register } from '../views/register.js';
+import { registerForm } from '../views/register.js';
 
 export default () => {
-  const divRegister = '';
-  const buttonRegister = register.divRegister.querySelector('.button-register');
-  buttonRegister.addEventListener('click', () => {
-    window.location.hash = '/home';
+  const divRegister = document.createElement('div');
+  divRegister.innerHTML = registerForm;
+
+  const linkRegister = divRegister.querySelector('.button-register');
+  linkRegister.addEventListener('click', () => {
+    window.location.hash = '/register';
   });
+
+  const formRegister = divRegister.querySelector('.formRegister');
+  formRegister.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const user = formRegister.user.value;
+    const email = formRegister.email.value;
+    const password = formRegister.password.value;
+
+    if (user && email && password) {
+      return createUser(user, email, password);
+    }
+  });
+
+  const createUser = (user, email, password) => {
+    console.log('creando usuario con nombre:', user);
+
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+      .then((user) => {
+        console.log('usuario creado');
+      })
+      .catch((error => console.error(error)));
+  };
 
   return divRegister;
 };
