f0f59e621e3d46118b40702dd2eee55c3407c0a8
silvanarl <ramossilvana495@gmail.com>
2020-06-02 12:54:14 -0500

    creado form de registro

--- src/views/register.js
+++ src/views/register.js
@@ -1,13 +1,18 @@
-const register = () => {
-  const registerForm = `<input type="text" placeholder="Nombre de Usuario" class ="input">
-  <input type="email" placeholder="E-mail" class ="input">
-  <input type="password" placeholder="Contraseña" class ="input">
-  <button class="button-register">Registrarme</button>
-  `;
+const registerForm = `
+<div class ="container-register">
+  <img src="img/bagtravel.png" alt="icon-roadTips">
+  <img src="img/logo.svg" alt="logo-road-tips">
+  <form name="formRegister" class="formRegister">
+    <label for="formRegister">¡Bienvenidx usuarix!</label>
+      <input type="text" name="user" placeholder="Nombre de Usuario" class ="input">
+      <input type="email" name="email" placeholder="E-mail" class ="input" required/>
+      <input type="password" name="password" placeholder="Contraseña" class ="input" required/>
+      <div class="container-button-register">
+        <button class="button-register">Registrarme</button>
+        <img src="" alt="sello">
+      </div>
+  </form>
+ </div> 
+`;
 
-  const divRegister = document.createElement('div');
-  divRegister.innerHTML = registerForm;
-  return divRegister;
-};
-
-export { register };
+export { registerForm };
