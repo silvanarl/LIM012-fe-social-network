6f310e8f274e099a7b58ecb0dbd1af5064a33e39
silvanarl <ramossilvana495@gmail.com>
2020-06-04 13:49:19 -0500

    corregido los botones

--- src/views/login.js
+++ src/views/login.js
@@ -7,23 +7,22 @@ const login = `
         <img id="bagtravelImage" src="img/bagtravel.png" alt="icon-roadTips">
         <img id ="logoImage" src="img/logo.svg" alt="logo-road-tips">
         <form name="loginRegister" id="loginRegister" class="container-form">
-            <label for="formGreeting">¡Bienvenidx usuarix!</label>
-              <input type="email" name="email" placeholder="E-mail" class ="input" required/>
-              <input type="password" name="password" placeholder="Contraseña" class ="input" required/>
-              <button class="button-login">Iniciar sesión</button>
-                <div class="option">
-                  <span class="line"></span>
-                  <span id="or">O</span>
-                  <span class="line"></span>
-                </div>
-                <div class="loginGoogle">
-                  <button id="button-google">Continuar con google</button>
-                  <img id="google-icon" src="img/selloGmail.svg" alt="selloGoogle" />
-                </div>
-              <span>¿No tienes cuenta? <a class="link" href="#/register">Regístrate</a></span>
-        </form>
+          <label for="formGreeting">¡Bienvenidx usuarix!</label>
+            <input type="email" name="email" placeholder="E-mail" class ="input" required/>
+            <input type="password" name="password" placeholder="Contraseña" class ="input" required/>
+            <button type="submit" class="button-login">Iniciar sesión</button>
+              <div class="option">
+                <span class="line"></span>
+                <span id="or">O</span>
+                <span class="line"></span>
+              </div>
+            
+              <button type="button" id="button-google">Continuar con google</button>
+              <img id="google-icon" src="img/selloGmail.svg" alt="selloGoogle" />
+         
+          <span>¿No tienes cuenta? <a class="link" href="#/register">Regístrate</a></span> 
+        </form>            
   </div>
-  
- </div>`;
+</div>`;
 
 export { login };
