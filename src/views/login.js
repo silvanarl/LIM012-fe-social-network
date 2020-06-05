1c331042eb3b2828820c1876a1675aea50985998
silvanarl <ramossilvana495@gmail.com>
2020-06-05 15:57:06 -0500

    modiflog

--- src/views/login.js
+++ src/views/login.js
@@ -16,10 +16,8 @@ const login = `
                 <span id="or">O</span>
                 <span class="line"></span>
               </div>
-            
               <button type="button" id="button-google">Continuar con google</button>
               <img id="google-icon" src="img/selloGmail.svg" alt="selloGoogle" />
-         
           <span>¿No tienes cuenta? <a class="link" href="#/register">Regístrate</a></span> 
         </form>            
   </div>
