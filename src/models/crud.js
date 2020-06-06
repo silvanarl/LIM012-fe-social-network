a7fd97e8ad872fb67df9065638c59a58ebba959c
silvanarl <ramossilvana495@gmail.com>
2020-06-05 20:40:53 -0500

    crud
    archivo para crear, editar, actualizar y eliminar post

---
+++ src/models/crud.js
@@ -0,0 +1,12 @@
+// import { setupPosts } from '../controllers/home.controller.js';
+
+// const dataPost = () => {
+//   console.log('algo');
+//   firebase.firestore().collection('posts')
+//     .get()
+//     .then((snapshot) => {
+//       console.log(snapshot.docs);
+//     });
+// };
+
+// export { dataPost };
