e3430f2f12b596c5964acd5969303a41d889a324
silvanarl <ramossilvana495@gmail.com>
2020-06-05 20:36:26 -0500

    agregada estructura home

--- src/views/home.js
+++ src/views/home.js
@@ -1,5 +1,37 @@
-const home = name => `
-  <div>Home ${name} </div>
+const home = `
+<header>
+  <div class="header">
+    <div>
+      <span> name user</span>
+      <img src="" alt="icon-menu">
+    </div>
+    <img id ="logoImage" src="img/logo.svg" alt="logo-road-tips">
+    <span>Cerrar Sesión</span>
+  </div>
+</header>
+<main>
+  <div class="profile">
+    <img src="" alt="">
+    <img src="" alt="img-profile">
+    <img src="" alt="icon-travel">
+  </div>
+  <div class="post-createPost">
+    <div class="container-createPost">
+      <div>
+        <input type="text" placeholder="¡Comparte tus tips aquí!">
+      </div>
+      <img src="" alt="createPostAddImage">
+      <img src="" alt="createPostPublic">
+      <img src="" alt="createPostPrivate">
+      <button class="button-createPost">Publicar</button>
+    </div>
+    <div class="posts"
+      <ul class = "postPublic">
+
+      </ul>
+    </div>
+  </div>
+</main>
 `;
 
 export { home };
