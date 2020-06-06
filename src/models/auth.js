5353875037fd74020c4f09fdb0661b4fcb5cb4da
silvanarl <ramossilvana495@gmail.com>
2020-06-05 20:35:10 -0500

    add func de sesion activa

--- src/models/auth.js
+++ src/models/auth.js
@@ -1,13 +1,13 @@
-const loginUser = (inputEmail, InputPassword) =>
-  // console.log('inicio de sesion con', email);
-  firebase
-    .auth()
-    .signInWithEmailAndPassword(inputEmail, InputPassword)
-    .then((user) => {
-      window.location.hash = '/home';
-      return user;
-    })
-    .catch((error) => console.error(error));
+// import { dataPost } from './crud.js';
+
+const loginUser = (inputEmail, InputPassword) => firebase
+  .auth()
+  .signInWithEmailAndPassword(inputEmail, InputPassword)
+  .then((user) => {
+    window.location.hash = '/home';
+    return user;
+  })
+  .catch(error => console.error(error));
 
 const loginWithGoogle = () => {
   const provider = new firebase.auth.GoogleAuthProvider();
@@ -21,4 +21,16 @@ const loginWithGoogle = () => {
     });
 };
 
-export { loginUser, loginWithGoogle };
+const userStatus = () => {
+  firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+      firebase.firestore().collection('posts')
+        .get()
+        .then((snapshot) => {
+          console.log(snapshot.docs);
+        });
+    }
+  });
+};
+
+export { loginUser, loginWithGoogle, userStatus };
