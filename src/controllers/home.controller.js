8c9819c12beb0589cc75c182f6b96f8b69437610
silvanarl <ramossilvana495@gmail.com>
2020-06-05 19:37:43 -0500

    post and home
    separada fun default a mostar home y mostrar post

--- src/controllers/home.controller.js
+++ src/controllers/home.controller.js
@@ -1,11 +1,34 @@
 import { home } from '../views/home.js';
 
-export default () => {
+const showDiv = () => {
   const divElement = document.createElement('div');
+  divElement.innerHTML = home;
 
-  const user = firebase.auth().currentUser;
-
-  divElement.innerHTML = home(user.email);
+  // Posts
 
   return divElement;
 };
+
+const postList = document.querySelector('.postPublic');
+const setupPosts = (data) => {
+  let postData = '';
+  if (data.length) {
+    data.forEach((doc) => {
+      const post = doc.data();
+      const li = `
+      <li class="postList">
+        <h5>${post.title}</h5>
+        <span>${post.date}</span>
+        <p>${post.content}</p>
+      </li>
+    `;
+      postData += li;
+    });
+    postList.innerHTML = postData;
+  } else {
+    postList.innerHTML = '<h4 class="text-white">Login to See Posts</h4>';
+  }
+  return postData;
+};
+
+export { setupPosts, showDiv };
