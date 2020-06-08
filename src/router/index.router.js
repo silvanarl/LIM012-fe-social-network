c467adb5ab447d93d8726fba89afc38c4dceff08
silvanarl <ramossilvana495@gmail.com>
2020-06-08 18:39:20 -0500

    switch

--- src/router/index.router.js
+++ src/router/index.router.js
@@ -1,20 +1,18 @@
 import { pages } from '../controllers/index.js';
-import { auth } from '../firebase.config.js';
+// import { auth } from '../firebase.config.js';
 
 const router = async (route) => {
   const content = document.getElementById('root');
   content.innerHTML = '';
 
-  var user = firebase.auth().currentUser;
+  const user = firebase.auth().currentUser;
   console.log('user', user);
   switch (route) {
     case '#/': {
       return content.appendChild(pages.loginUserWithEmail());
     }
     case '#/home': {
-      console.log('logged', user.email);
-      if (user.email != null) return content.appendChild(await pages.home());
-      window.location.hash = '/';
+      return content.appendChild(await pages.home());
     }
     case '#/register': {
       return content.appendChild(pages.register());
