eefb23481a2eb46ec948ded2fe24fec0fcc40398
silvanarl <ramossilvana495@gmail.com>
2020-06-08 01:16:01 -0500

    register

--- src/views/register.js
+++ src/views/register.js
@@ -5,7 +5,7 @@ const registerForm = `
   </div>
   <div class="container-form">
     <img id= "bagtravelImage" src="img/bagtravel.png" alt="icon-roadTips">
-    <img id ="logoImage" src="img/logo.svg" alt="logo-road-tips">
+    <img id ="logoImage" src="img/logo-roadtips.svg" alt="logo-road-tips">
     <form name="formRegister" class="container-form formRegister">
       <label for="formGreeting" >¡Registrate en nuestra red de viajerxs!</label>
         <input type="text" name="user" placeholder="Nombre de Usuario" class ="input">
