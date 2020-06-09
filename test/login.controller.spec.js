696c5bf25d9f2821c84ed203d363b712523b60bb
silvanarl <ramossilvana495@gmail.com>
2020-06-08 19:15:28 -0500

    test

--- test/login.controller.spec.js
+++ test/login.controller.spec.js
@@ -1,26 +1,11 @@
-import loginUser from '../src/models/auth.js';
-// import firebasemock from '../__mocks__/firebase-mock';
-const firebasemock = require('firebase-mock');
-
-const mockauth = new firebasemock.MockFirebase();
-mockauth.autoFlush();
-global.firebase = firebasemock.MockFirebaseSdk(
-  // use null if your code does not use RTDB
-  () => null,
-  () => mockauth,
-);
-
+import { loginUser } from '../src/models/auth.js';
+import '../__mocks__/functionMock.js';
 
 describe('loginUser', () => {
   it('debería ser una función', () => {
     expect(typeof loginUser).toBe('function');
   });
   it('deberia registar un usuario con correo y password', (done) => {
-    // firebasemock.create({
-    //   user: 'ben',
-    //   email: 'ben@example.com',
-    //   password: 'examplePass',
-    // });
     loginUser('ben@example.com', 'examplePass').then((user) => {
       expect(user.email).toBe('ben@example.com');
       done();
