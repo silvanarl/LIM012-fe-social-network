cecb157632b39461cd28e15a1700bbea10390b5c
silvanarl <ramossilvana495@gmail.com>
2020-06-09 00:24:28 -0500

    clases agregadas y src de iconos

--- src/views/home.js
+++ src/views/home.js
@@ -2,36 +2,37 @@ const home = () => `
 <header>
   <div class="header">
     <div>
-      <span> name user</span>
-      <img src="img/icon-triangleProfile.svg" alt="icon-triangle-menu">
-      <img src="img/icon-menu.svg" class="hide" alt="icon-menu">
+      <span class="hide"> name user</span>
+      <img src="img/icon-triangleProfile.svg" class="hide"alt="icon-triangle-menu">
+      <img src="img/icon-menu.svg" class="icon-menu" alt="icon-menu">
     </div>
     <img id ="logoImage" src="img/logo-roadtips.svg" alt="logo-road-tips">
-    <a href="#" class="logout"><span>Cerrar Sesión</span></a>
+    <a href="#" class="hide"><span>Cerrar Sesión</span></a>
   </div>
 </header>
 <main>
   <div class ="main">
     <div class="profile">
-      <img src="" alt="">
-      <img src="" alt="img-profile">
-      <img src="" alt="icon-travel">
+      <img src="" class="hide" alt="">
+      <img src="img/icon-color-user.svg" class="hide" alt="img-profile">
+      <img src="" class="hide" alt="icon-travel">
     </div>
     <div class="post-createPost">
       <div class="container-createPost">
         <div>
-          <input class="createPost" type="text" placeholder="¡Comparte tus tips aquí!">
+          <input class="createPost" type="text" size="30" placeholder="¡Comparte tus tips aquí!">
         </div>
-        <img src="" alt="createPostAddImage">
-        <img src="" alt="createPostPublic">
-        <img src="" alt="createPostPrivate">
-        <button class="button-createPost">Publicar</button>
+        <div class="icons-createPost">
+          <img src="img/icon-addImage.svg" class="icon-addImage" alt="createPostAddImage">
+          <img src="img/icon-world.svg" class="icon-world" alt="createPostPublic">
+          <img src="img/icon-privacy.svg" class="hide" alt="createPostPrivate">
+          <button class="button-createPost">Publicar</button>
+        </div>
       </div>
       <div class="posts">
         <ul class="postPublic" id="publicPost">
 
-          </ul>
-        
+        </ul>
       </div>
     </div>
   </div>
