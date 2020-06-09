526fc6ca6164a61aab0603d92b44b878fb0636a0
silvanarl <ramossilvana495@gmail.com>
2020-06-09 00:18:42 -0500

    funcion cambiada a este file desde firebasemock

---
+++ __mocks__/mock.js
@@ -0,0 +1,9 @@
+import firebasemock from 'firebase-mock';
+
+console.log(firebasemock);
+const mockauth = new firebasemock.MockAuthentication();
+mockauth.autoFlush();
+global.firebase = firebasemock.MockFirebaseSdk(
+  () => null,
+  () => mockauth,
+);
