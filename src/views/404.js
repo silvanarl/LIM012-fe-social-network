57f2e810750c708f9c8c3c78e736415eab7a3972
silvanarl <ramossilvana495@gmail.com>
2020-06-10 00:47:04 -0500

    vistas agregadas
    home modificado, 404 agregado, post

--- src/views/404.js
+++ src/views/404.js
@@ -1,9 +1,14 @@
 export default () => {
   const view404 = `
-    <h2>404</h2>
+  <header>
+    <div class="header">
+      <img id ="logoImage" src="img/logo-roadtips.svg" alt="logo-road-tips"/>
+    </div>
+  </header>  
+    <img class="imgNoFound" src="img/img404.png" alt="image page no found"/>
     <h1>Página no encontrada</h1>
     <p>El archivo especificado no se encontró en este sitio web. Por favor, compruebe la URL para errores y vuelva a intentarlo.</p>
-    `;
+  `;
 
   const divElemt = document.createElement('div');
   divElemt.setAttribute('id', 'message');
---
+++ src/views/posts.js
@@ -0,0 +1,17 @@
+const post = data => `
+<div class="container-post">
+    <div class="user-post">
+        <span class="titleUserPost">${data.title}</span>
+        <span class="timeUserPost">${data.date}</span>
+    </div>
+    <div class="content-post">
+        <p>${data.content}</p>
+        <div class="likeAndCommentPost">
+            <img src="img/icon-bagLike.svg" class="icon-contentPost" alt="icon like">
+            <img src="img/icon-comments.svg" class="icon-contentPost" alt="icon comments">
+        </div>
+    </div>
+</div>
+`;
+
+export { post };
