7de702dd07d38b98ac3d4193c8bc65807ce23a84
silvanarl <ramossilvana495@gmail.com>
2020-06-11 01:02:30 -0500

    agregado crear comentarios

--- src/views/posts.js
+++ src/views/posts.js
@@ -8,10 +8,32 @@ const post = data => `
         <p>${data.content}</p>
         <div class="likeAndCommentPost">
             <img src="img/icon-bagLike.svg" class="icon-contentPost" alt="icon like">
+            <span class="counterLikes">1</span>
             <img src="img/icon-comments.svg" class="icon-contentPost" alt="icon comments">
+            <span class="counterComments">1</span>
+            <div class="createComment">
+                <input class="textComment" type="text" placeholder="Comentar...">
+                <img src="img/iconSend.svg" class="iconSend" alt="icon send comment">
+            </div>
+            <div class="contentComment">
+                
+            </div>
         </div>
     </div>
 </div>
 `;
 
-export { post };
+const comment = dataComment => `
+<div class="containerComments">
+    <div class="user-photo">
+        <img src="img/icon-color-user.svg" alt="image user comment">
+    </div>
+    <div class="mainComment">
+        <span class="userComment">${dataComment.title}</span>
+        <span class="contentComment">${dataComment.content}</span>
+    </div>
+    <span class="timeComment">${dataComment.date}</span>
+</div>
+`;
+
+export { post, comment };
