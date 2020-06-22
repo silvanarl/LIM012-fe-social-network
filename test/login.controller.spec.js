9b8b96fa8d1759771c4f11fdd65e507bed0fa0f9
silvanarl <ramossilvana495@gmail.com>
2020-06-22 00:07:02 -0500

    tests createUser

--- test/login.controller.spec.js
+++ test/login.controller.spec.js
@@ -1,5 +1,17 @@
 import '../__mocks__/functionMock.js';
-import { loginUser } from '../src/models/auth.js';
+import { loginUser, createUser } from '../src/models/auth.js';
+
+describe('createUser', () => {
+  it('debería ser una función', () => {
+    expect(typeof createUser).toBe('function');
+  });
+  it('deberia crear un usuario con nombre de usuario, correo y password', (done) => {
+    createUser('Ben', 'ben@example.com', 'examplePass').then((user) => {
+      expect(user.email).toBe('ben@example.com');
+      done();
+    });
+  });
+});
 
 describe('loginUser', () => {
   it('debería ser una función', () => {
