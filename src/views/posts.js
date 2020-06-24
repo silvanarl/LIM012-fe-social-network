430409172f36ec0f9a7810c0888811b928269563
silvanarl <ramossilvana495@gmail.com>
2020-06-23 22:14:17 -0500

    span vacío counterComments

--- src/views/posts.js
+++ src/views/posts.js
@@ -29,7 +29,7 @@ const post = (data) => {
             <button class="btnLikes"><img src="img/icon-bagLike.svg" class="icon-contentPost" alt="icon like"></button>
             <span class="numberLikes">${data.likesUsers.length}</span>
             <button class="btnComments"><img src="img/icon-comments.svg" class="icon-contentPost" alt="icon comments"></button>
-            <span class="counterComments">1</span>
+            <span class="counterComments"></span>
             <div class="createComment hide">
                 <input class="textComment" type="text" placeholder="Comentar...">
                 <button class="buttonSend"><img src="img/iconSend.svg" class="iconSend" alt="icon send comment"></button>
