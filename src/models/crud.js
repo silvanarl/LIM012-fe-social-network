f2ca5ccf744b8469f8796a0257cd185ba6bab3ee
luzcry <mcrodriguezt618@gmail.com>
2020-07-01 00:35:22 -0500

    merging commit

--- src/models/crud.js
+++ src/models/crud.js
@@ -37,31 +37,6 @@ const getComments = onSnapshot => firebase.firestore()
   .orderBy('date', 'desc')
   .onSnapshot(onSnapshot);
 
-// const getComments = async () => {
-//   const comments = [];
-//   await firebase
-//     .firestore()
-//     .collection('comments')
-//     .orderBy('date', 'desc')
-//     .get()
-//     .then((querySnapshot) => {
-//       console.log(querySnapshot);
-//       querySnapshot.forEach((doc) => {
-//         const commentData = {
-//           id: doc.id,
-//           photo: doc.data().photo,
-//           author: doc.data().author,
-//           content: doc.data().content,
-//           date: doc.data().date.toDate().toLocaleString(),
-//           userID: doc.data().userID,
-//           postID: doc.data().postID,
-//         };
-//         comments.push(commentData);
-//       });
-//     });
-//   return comments;
-// };
-
 const createComment = ({
   photo, author, content, postID,
 }) => {
--- test/post.controller.spec.js
+++ test/post.controller.spec.js
@@ -1,10 +1,12 @@
+import { resolvePlugin } from '@babel/core';
 import { createPost } from '../models/crud.js';
+import '../__mocks__/functionMock.js';
+import firestoremock from 'firebase-mock/src/firebase';
+global.firebase = new MockFirebase(fixtureData)
 
-const 
 describe('agregar post', () => {
-    it('Debería agregar un post'), () => {
-        return createPost('').then(() =>{
-            expect().toBe();
-        })
-    }
-})
+  it('Debería agregar un post'),
+  () => createPost('hola mundo').then((data) => {
+    expect(data).toBe('el post fue agregado');
+  });
+});
